f16e67c7ef131f1b125dd7d9b127d66c57bbcf33
nahuel36 <nahuel36gg@gmail.com>
2022-04-23 20:56:00 -0300

    adapter para path finding

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface PathFinder
+{
+    void WalkTo(Vector3 destiny);
+
+    bool Reached { get; }
+
+}
