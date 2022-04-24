e98fd210326edb45459be359de71bde08dcc41cf
nahuel36 <nahuel36gg@gmail.com>
2022-04-23 23:22:36 -0300

    probando pathfinder en la escena

---
+++ Assets/Scripts/AStarPathFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AStarPathFinder : MonoBehaviour, IPathFinder
+{
+    // Start is called before the first frame update
+    public void WalkTo(Vector3 destiny)
+    {
+        
+    }
+
+    // Update is called once per frame
+    public bool Reached
+    {
+        get { return true; }
+    }
+}
