bbaf5436ad50b6654b07110074446e74be892c01
nahuel36 <nahuel36gg@gmail.com>
2022-05-22 22:11:33 -0300

    caminata cancelable

--- Assets/Scripts/CharacterWalk.cs
+++ Assets/Scripts/CharacterWalk.cs
@@ -7,23 +7,35 @@ public class CharacterWalk : IInteraction
 {
     IPathFinder pathfinder;
     Vector3 destiny;
-
+    bool isCancelable;
+    bool cancel;
     // Start is called before the first frame update
     public async Task Execute()
     {
         pathfinder.WalkTo(destiny);
 
+        cancel = false;
+
         await Task.Delay(1000);
 
-        while (pathfinder.Reached == false)
+        while (pathfinder.Reached == false && !cancel)
             await Task.Yield();
     }
 
     // Update is called once per frame
-    public void Queue(IPathFinder pathfinder, Vector3 destiny)
+    public void Queue(IPathFinder pathfinder, Vector3 destiny, bool cancelable = false)
     {
+        this.isCancelable = cancelable;
         this.pathfinder = pathfinder;
         this.destiny = destiny;
         InteractionManager.Instance.AddCommand(this);
+    }
+
+    public void Cancel()
+    {
+        if(isCancelable)
+        {
+            cancel = true;
+        }
     }
 }
