12e2c0f024acc93ae8a98d85e5e563f256113e82
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-05-23 18:49:42 -0300

    pasadas clases de interaccion y de variables a los padres correspondientes

--- Assets/Scripts/PNCVariablesContainer.cs
+++ Assets/Scripts/PNCVariablesContainer.cs
@@ -2,20 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class InteractuableLocalVariable
+{
+    public string name = "new variable";
+    public int integer = 0;
+    public bool integerDefault = true;
+    public bool boolean = false;
+    public bool booleanDefault = true;
+    public string String = "";
+    public bool stringDefault = true;
+    [System.Flags]
+    public enum types
+    {
+        integer = (1 << 0),
+        boolean = (1 << 1),
+        String = (1 << 2)
+    }
+    public types type;
+    public int globalHashCode = -1;
+    public bool expandedInInspector;
+}
+
+[System.Serializable]
+public class InteractuableGlobalVariable
+{
+    public string name = "new variable";
+    public int integer = 0;
+    public bool boolean = false;
+    public string String = "";
+    public int globalHashCode = -1;
+    public GlobalVariableProperty properties;
+    public bool expandedInInspector;
+}
+
 public class PNCVariablesContainer : MonoBehaviour
 {
     public InteractuableLocalVariable[] local_variables = new InteractuableLocalVariable[0];
     public InteractuableGlobalVariable[] global_variables = new InteractuableGlobalVariable[0];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
