9f14c026d07eba2db03b0d0ee8ca6caa73d0b586
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-05-26 03:16:54 -0300

    falto renombrar en un lugar

--- Assets/Scripts/Inventory/InventoryList.cs
+++ Assets/Scripts/Inventory/InventoryList.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Inventory", menuName = "Pnc/InventoryContainer", order = 1)]
+[CreateAssetMenu(fileName = "Inventory", menuName = "Pnc/InventoryList", order = 1)]
 public class InventoryList : ScriptableObject
 {
     public InventoryItem[] items;
