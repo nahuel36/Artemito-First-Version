5cb21f948da8f819ef95d2c0ed0ea255f3847f71
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-07-26 20:17:39 -0300

    ahora el walkable area con el respectivo pathfinding se crea al darle play

--- Assets/Scripts/Walk/Editor/WalkableArea2DEditor.cs
+++ Assets/Scripts/Walk/Editor/WalkableArea2DEditor.cs
@@ -33,70 +33,9 @@ public class WalkableArea2DEditor : Editor
         {
             Selection.activeObject = settings;
         }
-        if (GUILayout.Button("Create PathFinder"))
-        {
-            for (int i = myTarget.transform.childCount-1; i >= 0; i--)
-                DestroyImmediate(myTarget.transform.GetChild(i).gameObject);
-
-
-            Collider2D collider = myTarget.GetComponent<Collider2D>();
-
-            Settings settings = Resources.Load<Settings>("Settings/Settings");
-#if NAVMESH_PLUS
-            for (int i = myTarget.GetComponents<NavMeshPlus.Components.NavMeshModifier>().Length-1; i >= 0 ; i--)
-            {
-                DestroyImmediate(myTarget.GetComponents<NavMeshPlus.Components.NavMeshModifier>()[i]);
-            }
-            if (FindObjectOfType<NavMeshPlus.Components.NavMeshSurface>())
-                DestroyImmediate(FindObjectOfType<NavMeshPlus.Components.NavMeshSurface>().gameObject);
-#endif
-#if ASTAR_ARONGRANBERG_PATHFINDING
-            if (AstarPath.active)
-                DestroyImmediate(AstarPath.active.gameObject);
-#endif
-            WalkObstacle[] obstacles = FindObjectsOfType<WalkObstacle>();
-            for (int i = 0; i < obstacles.Length; i++)
-            {
-                obstacles[i].Init();
-            }
-
-            if (settings.pathFindingType == Settings.PathFindingType.AronGranbergAStarPath)
-            {
-#if ASTAR_ARONGRANBERG_PATHFINDING
-                GameObject pathGO = Instantiate(Resources.Load<GameObject>("Prefabs/AStarPathFinderPrefab"), myTarget.transform);
-                AstarPath.FindAstarPath();
-                ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph)))
-                    .SetDimensions(
-                        Mathf.RoundToInt(collider.bounds.size.x / myTarget.node_size),
-                        Mathf.RoundToInt(collider.bounds.size.y / myTarget.node_size),
-                        myTarget.node_size);
-                ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph))).center = collider.offset + (Vector2)myTarget.transform.position;
-                ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph))).Scan();
-
-                Selection.activeGameObject = pathGO;
-#endif
-            }
-            if (settings.pathFindingType == Settings.PathFindingType.UnityNavigationMesh)
-            {
-#if NAVMESH_PLUS
-                myTarget.gameObject.AddComponent<NavMeshPlus.Components.NavMeshModifier>();
-
-                GameObject GO = new GameObject("NavMeshManager");
-                GO.transform.rotation = Quaternion.Euler(-90, 0, 0);
-                NavMeshPlus.Components.NavMeshSurface surface = GO.AddComponent<NavMeshPlus.Components.NavMeshSurface>();
-                GO.AddComponent<NavMeshPlus.Extensions.CollectSources2d>();
-
-                surface.useGeometry = NavMeshCollectGeometry.PhysicsColliders;
-
-                surface.AddData();
-                surface.BuildNavMesh();
-                surface.UpdateNavMesh(surface.navMeshData);
-
-                GO.transform.parent = myTarget.transform;
-
-#endif                
-            }
-        }
+       
+            
+        
 
 
     }
--- Assets/Scripts/Walk/WalkableArea2D.cs
+++ Assets/Scripts/Walk/WalkableArea2D.cs
@@ -8,4 +8,68 @@ public class WalkableArea2D : MonoBehaviour
 
     public float node_size = 1;
 
+    private void Start()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+            DestroyImmediate(transform.GetChild(i).gameObject);
+
+
+        Collider2D collider = GetComponent<Collider2D>();
+
+        Settings settings = Resources.Load<Settings>("Settings/Settings");
+#if NAVMESH_PLUS
+        for (int i = GetComponents<NavMeshPlus.Components.NavMeshModifier>().Length - 1; i >= 0; i--)
+        {
+            DestroyImmediate(GetComponents<NavMeshPlus.Components.NavMeshModifier>()[i]);
+        }
+        if (FindObjectOfType<NavMeshPlus.Components.NavMeshSurface>())
+            DestroyImmediate(FindObjectOfType<NavMeshPlus.Components.NavMeshSurface>().gameObject);
+#endif
+#if ASTAR_ARONGRANBERG_PATHFINDING
+        if (AstarPath.active)
+            DestroyImmediate(AstarPath.active.gameObject);
+#endif
+        WalkObstacle[] obstacles = FindObjectsOfType<WalkObstacle>();
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            obstacles[i].Init();
+        }
+
+        if (settings.pathFindingType == Settings.PathFindingType.AronGranbergAStarPath)
+        {
+#if ASTAR_ARONGRANBERG_PATHFINDING
+            GameObject pathGO = Instantiate(Resources.Load<GameObject>("Prefabs/AStarPathFinderPrefab"), transform);
+            AstarPath.FindAstarPath();
+            ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph)))
+                .SetDimensions(
+                    Mathf.RoundToInt(collider.bounds.size.x / node_size),
+                    Mathf.RoundToInt(collider.bounds.size.y / node_size),
+                    node_size);
+            ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph))).center = collider.offset + (Vector2)transform.position;
+            ((Pathfinding.GridGraph)AstarPath.active.data.FindGraphOfType(typeof(Pathfinding.GridGraph))).Scan();
+#endif
+        }
+        if (settings.pathFindingType == Settings.PathFindingType.UnityNavigationMesh)
+        {
+#if NAVMESH_PLUS
+            gameObject.AddComponent<NavMeshPlus.Components.NavMeshModifier>();
+
+            GameObject GO = new GameObject("NavMeshManager");
+            GO.transform.rotation = Quaternion.Euler(-90, 0, 0);
+            NavMeshPlus.Components.NavMeshSurface surface = GO.AddComponent<NavMeshPlus.Components.NavMeshSurface>();
+            GO.AddComponent<NavMeshPlus.Extensions.CollectSources2d>();
+
+            surface.useGeometry = UnityEngine.AI.NavMeshCollectGeometry.PhysicsColliders;
+
+            surface.AddData();
+            surface.BuildNavMesh();
+            surface.UpdateNavMesh(surface.navMeshData);
+
+            GO.transform.parent = transform;
+
+#endif
+        }
+
+    }
+
 }
