5dee9de4e890dcf09f2bc8b629f09777e396d22b
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-08-02 21:39:48 -0300

    ahora si, arreglado del todo la cabeza girada

--- Assets/Scripts/Animation/CharacterAnimator.cs
+++ Assets/Scripts/Animation/CharacterAnimator.cs
@@ -33,6 +33,8 @@ public class CharacterAnimator : MonoBehaviour
         animator = anim;
         character = charact;
         configured = true;
+
+        animator.SetAngle(angle);
     }
     
     // Update is called once per frame
