5b78ed06a5ed317c6757677fce1b186096697c46
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-08-07 08:47:13 -0300

    que los executed times de unhandled events se reinicien a 0

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class GameInitializer : MonoBehaviour
 {
     // Start is called before the first frame update
+    public UnhandledEvents unhandledEvents;
+
     void Start()
     {
         DialogsManager.Instance.Initialize();
         InventoryManager.Instance.Initialize();
 
+        unhandledEvents = Resources.Load<UnhandledEvents>("UnhandledEvents");
+        for (int i = 0; i < unhandledEvents.verbs.Count; i++)
+        {
+            unhandledEvents.verbs[i].attempsContainer.executedTimes = 0;
+        }
+        for (int i = 0; i < unhandledEvents.inventoryActions.Count; i++)
+        {
+            unhandledEvents.inventoryActions[i].attempsContainer.executedTimes = 0;
+        }
     }
 }
 
