07ace7b6c7fefe7401f1486b3e2e542da78afc13
NahuelHawk <nmuchetti@purpletreestudio.com>
2023-08-08 21:02:52 -0300

    obtener nombre de interactuable por id

--- Assets/Examples/SayForInventory.cs
+++ Assets/Examples/SayForInventory.cs
@@ -6,11 +6,20 @@ public class SayForInventory : MonoBehaviour
 {
     public void Say(List<CustomArgument> arguments)
     {
+        PNCInteractuable interactuable = null;
+        foreach (PNCInteractuable inter in FindObjectsOfType<PNCInteractuable>())
+        {
+            if (inter.GetInstanceID() == arguments[0].interactuableID)
+            {
+                interactuable = inter;
+            }
+        }
+
         foreach (PNCCharacter charact in FindObjectsOfType<PNCCharacter>())
         {
             if (charact.isPlayerCharacter)
             {
-                charact.Talk(arguments[0].stringArgument);
+               charact.Talk(arguments[0].stringArgument + " " + interactuable.interactuableName);
             }
         }
     }
