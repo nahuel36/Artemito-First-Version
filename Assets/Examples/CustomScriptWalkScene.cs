8d360c0fc2506b7573caa90ad6220db056f35220
nahuel36 <nahuel36gg@gmail.com>
2025-12-10 19:56:56 -0300

    Remove unused Start and Update methods
    Removed empty Start and Update methods.

--- Assets/Examples/CustomScriptWalkScene.cs
+++ Assets/Examples/CustomScriptWalkScene.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class CustomScriptWalkScene : CustomScript
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
