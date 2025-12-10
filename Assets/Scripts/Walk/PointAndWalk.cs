85ac29dcaf2545dd053898075daf1841b993f770
nahuel36 <nahuel36gg@gmail.com>
2025-12-10 20:05:00 -0300

    Remove empty Start method
    Removed empty Start method from PointAndWalk class.

--- Assets/Scripts/Walk/PointAndWalk.cs
+++ Assets/Scripts/Walk/PointAndWalk.cs
@@ -7,11 +7,7 @@ public class PointAndWalk : MonoBehaviour
     [SerializeField] PNCCharacter pNCCharacter;
     [SerializeField] Camera camera;
 
-    public void Start()
-    {
 
-        
-    }
 
     public void WalkUnCancelable(){
         Vector3 point = camera.ScreenToWorldPoint(Input.mousePosition);
